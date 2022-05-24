613bb8256d449f6472f22ae55703e763f378857a
Tim <coolrazor007@yahoo.com>
2022-05-23 21:30:49 -0700

    cleaned up the auto testing script

--- automated_testing.py
+++ automated_testing.py
@@ -2,24 +2,25 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import unittest
 import requests
-page = requests.get("http://localhost:8080")
 
-soup = BeautifulSoup(page.content, 'html.parser')
-print(soup.prettify())
+# page = requests.get("http://localhost:8080")
 
-print("===SPACER1===")
+# soup = BeautifulSoup(page.content, 'html.parser')
+# print(soup.prettify())
 
-print(soup.find(id="indices-and-tables"))
+# print("===SPACER1===")
 
-indices = soup.find(id="indices-and-tables")
+# print(soup.find(id="indices-and-tables"))
 
-print("===SPACER2===")
+# indices = soup.find(id="indices-and-tables")
 
-print(indices.h1.prettify())
+# print("===SPACER2===")
 
-print("===SPACER3===")
+# print(indices.h1.prettify())
 
-print(indices.h1.get_text())
+# print("===SPACER3===")
+
+# print(indices.h1.get_text())
 
 
 print("====== SPACER FOR TEST CASES ======")
@@ -27,15 +28,12 @@ print("====== SPACER FOR TEST CASES ======")
 class Test(unittest.TestCase):
    bs = None
    def setUpClass():
-      #url = '<a target="_blank" rel="nofollow" href="https://en.wikipedia.org/wiki/Python">https://en.wikipedia.org/wiki/Python</a>'
-      #url = '<a target="_blank" rel="nofollow" href="http://localhost:8080">http://localhost:8080</a>'
       url = 'http://localhost:8080'
       Test.bs = BeautifulSoup(urlopen(url), 'html.parser')
    def test_titleText(self):
       pageTitle = Test.bs.find('h1').get_text()
       self.assertEqual('Post-Graduate DevOps Program Capstone¶', pageTitle);
    def test_contentExists(self):
-      #content = Test.bs.find('div',{'section id':'indices-and-tables'})
       content = Test.bs.find(id="indices-and-tables")
       self.assertIsNotNone(content)
 if __name__ == '__main__':
