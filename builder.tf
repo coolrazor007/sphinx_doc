938f7f64da8c04fa5db9a2255bf1616ebe1b0357
Tim <coolrazor007@yahoo.com>
2022-05-31 14:10:07 -0700

    because it iterates we needs star

--- builder.tf
+++ builder.tf
@@ -181,7 +181,7 @@ resource "local_file" "inventorybuilder" {
   content = templatefile("template.tpl",
     {
       #builderIP = aws_instance.builder.*.public_ip
-      builderIP = aws_eip.myeip.public_ip
+      builderIP = aws_eip.myeip.*.public_ip
     }
   )
   filename                  = "inventory.cfg"
