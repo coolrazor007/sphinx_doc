06c4946ddfd65af7f2ea32f68d736e207c213435
Tim <coolrazor007@yahoo.com>
2022-05-31 15:37:02 -0700

    dupe vars issue

--- pipeline.tf
+++ pipeline.tf
@@ -1,20 +1,4 @@
-data "aws_ami" "latest-ubuntu" {
-most_recent = true
-owners = ["099720109477"] # Canonical
 
-  filter {
-      name   = "name"
-      values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-      #Had issues with SSH with Jammy below.  Maybe not required because it was erroring on my prototype code
-      # that is not actually used in "provisioner "remote-exec""
-      #values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]      
-  }
-
-  filter {
-      name   = "virtualization-type"
-      values = ["hvm"]
-  }
-}
 resource "aws_instance" "pipeline" {
     ami           = "${data.aws_ami.latest-ubuntu.id}"
     instance_type = "t2.micro"
---
+++ variables.tf
@@ -0,0 +1,18 @@
+data "aws_ami" "latest-ubuntu" {
+most_recent = true
+owners = ["099720109477"] # Canonical
+
+  filter {
+      name   = "name"
+      values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+      #Had issues with SSH with Jammy below.  Maybe not required because it was erroring on my prototype code
+      # that is not actually used in "provisioner "remote-exec""
+      #values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]      
+  }
+
+  filter {
+      name   = "virtualization-type"
+      values = ["hvm"]
+  }
+}
+
