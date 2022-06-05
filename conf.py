239b9a7a0b3590768cc0ad038c4d91eadabf74d9
Tim <coolrazor007@yahoo.com>
2022-06-04 20:58:47 -0700

    changed theme and body width

--- conf.py
+++ conf.py
@@ -48,7 +48,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'bizstyle'
+
+body_max_width = 'none'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
