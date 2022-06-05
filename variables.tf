23bef80402876704def65ae16bf28405765c81c3
Tim <coolrazor007@yahoo.com>
2022-06-05 00:05:40 -0700

    added files to appendix updated comment on variables.tf

--- variables.tf
+++ variables.tf
@@ -5,9 +5,9 @@ owners = ["099720109477"] # Canonical
   filter {
       name   = "name"
       values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-      #Had issues with SSH with Jammy below.  Maybe not required because it was erroring on my prototype code
-      # that is not actually used in "provisioner "remote-exec""
-      #values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]      
+      # Had issues with SSH with Jammy below.  Maybe not required because it was erroring on my prototype code
+      # that is not actually used in the "provisioner "remote-exec""
+      # BREAKS:  values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]      
   }
 
   filter {
